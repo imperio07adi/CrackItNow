287dd0cea25bbd60f7546e19934ee2a3d64b3261
Aditya Kokashe <adi310kokashe@gmail.com>
2025-03-17 21:18:42 +0530

    Update scriptQ.js

--- scriptQ.js
+++ scriptQ.js
@@ -1,4 +1,3 @@
-// Function to toggle the answer visibility when clicking a question
 document.addEventListener("DOMContentLoaded", function () {
     const questionButtons = document.querySelectorAll(".question-btn");
 
@@ -6,14 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener("click", function () {
             const answer = this.nextElementSibling;
 
-            // Hide other answers if one is clicked
             document.querySelectorAll(".answer").forEach(ans => {
                 if (ans !== answer) {
                     ans.style.display = "none";
                 }
             });
 
-            // Toggle the clicked answer
             if (answer.style.display === "block") {
                 answer.style.display = "none";
             } else {
@@ -22,17 +19,14 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    // Function to get query parameters
     function getQueryParam(param) {
         const urlParams = new URLSearchParams(window.location.search);
         return urlParams.get(param);
     }
 
-    // Get company and interview type from URL
     const companyName = getQueryParam("company");
     const interviewType = getQueryParam("type");
 
-    // Update page dynamically
     if (companyName && interviewType) {
         document.getElementById("page-title").innerText = `${companyName} - ${interviewType}`;
         document.getElementById("company-interview-title").innerText = `Previous Year ${interviewType} Questions`;
