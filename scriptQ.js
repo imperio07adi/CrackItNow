3108a9ca6eda752cc0c286ec7f7567fcf1e67e36
Aditya Kokashe <adi310kokashe@gmail.com>
2025-04-02 00:44:23 +0530

    Add files via upload

--- scriptQ.js
+++ scriptQ.js
@@ -5,12 +5,14 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener("click", function () {
             const answer = this.nextElementSibling;
 
+            // hide other answers if one is clicked
             document.querySelectorAll(".answer").forEach(ans => {
                 if (ans !== answer) {
                     ans.style.display = "none";
                 }
             });
 
+            // toggle the clicked answer
             if (answer.style.display === "block") {
                 answer.style.display = "none";
             } else {
@@ -19,14 +21,17 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
+    // function to get query parameters
     function getQueryParam(param) {
         const urlParams = new URLSearchParams(window.location.search);
         return urlParams.get(param);
     }
 
+    // Get company and interview type from URL
     const companyName = getQueryParam("company");
     const interviewType = getQueryParam("type");
 
+    // Update page dynamically
     if (companyName && interviewType) {
         document.getElementById("page-title").innerText = `${companyName} - ${interviewType}`;
         document.getElementById("company-interview-title").innerText = `Previous Year ${interviewType} Questions`;
